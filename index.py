96d6e80ed033af7ca03973edc2ee25c73e5d0aae
Gavin Wiener <gavin.w@superbalist.com>
2017-08-26 01:17:58 +0200

    Flask template. Read the WhereIsMyTransport API key from the config.yml

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template
+import yaml
 app = Flask(__name__)
+
+secret = None
+clint_id = None
+with open("config.yml", 'r') as stream:
+    try:
+        config = yaml.load(stream)
+        secret = config['auth']['secret']
+        client_id = config['auth']['client-id']
+    except yaml.YAMLError as exc:
+        print(exc)
+
 
 @app.route('/')
 def main():
